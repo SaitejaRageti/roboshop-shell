18def9b44980c959acb1c978294198591a7ff1c1
saitejarageti <saitejarageti129@gmail.com>
2025-06-02 11:07:28 +0530

    hello world

--- frontend.sh
+++ frontend.sh
@@ -8,23 +8,39 @@ N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo "you are not running with root user"
+    echo "you are running with Non root user. Run with root user"
+    exit 1
 else
     echo "You are running with root user go ahead"
+fi
+
+dnf module disable nginx -y
+
+if [ $? -eq 0 ]
+then 
+    echo " disabled successfully"
+else
+    echo "disable failed"
     exit 1
 fi
 
-dnf module list ngnix
+dnf module enable nginx:1.24 -y
 
-if [$? -ne 0]
-then
-    echo "ngnix is not installed, will install now"
-    dnf install nginx -y
-    if [$? -eq 0]
-    then
-        echo "nginx installed successfully"
-    else
-        echo " nginx installation failed "
-else 
-    echo "nginx is already installed"
+if [ $? -eq 0 ]
+then 
+    echo " enabled successfully"
+else
+    echo "enable failed"
+    exit 1
 fi
+
+dnf install nginx -y 
+
+if [ $? -eq 0 ]
+then 
+    echo " installed successfully"
+else
+    echo "installation failed"
+    exit 1
+fi
+
