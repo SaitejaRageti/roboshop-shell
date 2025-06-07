d0c6d206881588a20e09b074c66015d6d4b00ce5
saitejarageti <saitejarageti129@gmail.com>
2025-06-07 15:17:14 +0530

    hello world

--- user.sh
+++ user.sh
@@ -68,7 +68,7 @@ cd /app
 npm install 
 validate $? "npm installing dependencies"
 
-cp $script_dir/user.service /etc/systemd/system/ 
+cp $script_dir/user.service /etc/systemd/system/user.service 
 validate $? "creating the user service file"
 
 systemctl daemon-reload &>>$logfile
